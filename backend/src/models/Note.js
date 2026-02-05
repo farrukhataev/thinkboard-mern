23979286e795dfc0e78e45c2e82115095a081e12
farrukhataev <farrukhataevl@gmail.com>
2026-02-05 22:14:02 +0300

    Setting MongoDB DataBase

---
+++ backend/src/models/Note.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const noteSchema = new mongoose.Schema(
+  {
+  title:{
+    type:String,
+    required:true
+  },
+  content:{
+    type:String,
+    required:true
+  },
+ 
+},
+{timestamps:true}//createdAt va updatedAt maydonlarini avtomatik qo'shadi
+);
+
+const Note = mongoose.model("Note", noteSchema);
+
+export default Note;
