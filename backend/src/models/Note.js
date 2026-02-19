c1afad8f2ef4905bdedaef6676814e6fb570839f
farrukhataev <farrukhataevl@gmail.com>
2026-02-19 21:01:15 +0300

    added delete and detail functionality

--- backend/src/models/Note.js
+++ backend/src/models/Note.js
@@ -1,18 +1,20 @@
 import mongoose from "mongoose";
 
+// 1st step: You need to create a schema
+// 2nd step: You would create a model based off of that schema
+
 const noteSchema = new mongoose.Schema(
   {
-  title:{
-    type:String,
-    required:true
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
   },
-  content:{
-    type:String,
-    required:true
-  },
- 
-},
-{timestamps:true}//createdAt va updatedAt maydonlarini avtomatik qo'shadi
+  { timestamps: true } // createdAt, updatedAt
 );
 
 const Note = mongoose.model("Note", noteSchema);
